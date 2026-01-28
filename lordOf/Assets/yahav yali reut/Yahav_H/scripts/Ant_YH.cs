4df2ba6a6cf8c6390e8e6bbe6a7f47cec7a668a4
yahav080 <127886412+yahav080@users.noreply.github.com>
2026-01-28 16:05:02 +0200

    Uptade

---
+++ lordOf/Assets/yahav yali reut/Yahav_H/scripts/Ant_YH.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Ant_YH : MonoBehaviour
+{
+    public GameObject body;
+    Animator anim;
+    public float speed = 3.0f;//מהירות הנמלה
+    public bool way;//הכיון שהנמלה זזה אליו
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = body.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (way == true)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else if (way == false)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "wall")
+        {
+            way = !way;
+        }
+
+        if (other.gameObject.tag == "player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
