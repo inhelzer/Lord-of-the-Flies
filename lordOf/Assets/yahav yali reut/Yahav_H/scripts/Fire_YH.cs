490aea58e58e1051442324ff9420b058e28f74e8
yahav080 <127886412+yahav080@users.noreply.github.com>
2026-02-25 16:46:39 +0200

    JHJHFS

---
+++ lordOf/Assets/yahav yali reut/Yahav_H/scripts/Fire_YH.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Fire_YH : MonoBehaviour
+{
+    public float speed = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Time.timeSinceLevelLoad * speed;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
