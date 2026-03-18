32bb083a6773732305ce3ec0c0fdf3bd7d2ea40f
liadmen54-design <liadmen54@gmail.com>
2026-03-18 14:19:05 +0200

    liad fixed level

--- lordOf/Assets/liad&raziel/L&R Scripts/Liad/Arrow_shooter.cs
+++ lordOf/Assets/liad&raziel/L&R Scripts/Liad/Arrow_shooter.cs
@@ -2,22 +2,24 @@ using UnityEngine;
 
 public class Arrow_shooter : MonoBehaviour
 {
+    GameObject player;
     [SerializeField]GameObject shoot;
-    [SerializeField] GameObject player;
+    [SerializeField] string playerName;
     float delay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        delay = Time.timeSinceLevelLoad;
+        player = GameObject.Find(playerName);
+        delay = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.x - gameObject.transform.position.x < 20f && player.transform.position.x - gameObject.transform.position.x > -20f && delay < Time.timeSinceLevelLoad)
+        if (player.transform.position.x - gameObject.transform.position.x < 20f && player.transform.position.x - gameObject.transform.position.x > -20f && delay < Time.time)
         {
             Instantiate(shoot, transform.position - new Vector3(1,0,0), Quaternion.Euler(0,0,90));
-            delay = Time.timeSinceLevelLoad + 1.5f;
+            delay = Time.time + 1.5f;
         }
     }
 }
